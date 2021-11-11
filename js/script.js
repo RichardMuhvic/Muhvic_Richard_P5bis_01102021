492b6537fb36622656fcf06375df718687dbfb4a
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-11 16:05:25 +0100

    ajotu paramètre 'item' dans al fonction

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-// 1-Récupérer les donnes de l'API avec fetch :
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         return res.json();
     })
-    .then(function(product) {
-        console.log(product);
+    .then(function(data) {
+        console.log(data);
+        product(data);
     });
 
-// 2-Faire une boucle "for" :
-for(let i=0; i<product.length; i++) {
-    console.log(product[i]);
 
-    //Intégrer un nouvel élément au DOM :
-    let element = document.querySelector("#items")
-    element.innerHTML = 
-    `<a href="./product.html?id=42">
-        <article>
-            <img src="${product[i].imageUrl}" alt="${product[i].altTxt}">
-            <h3 class="productName">${product[i].name}</h3>
-            <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-        </article>
-    </a>`;
-    
+function product(item) {
+    for(let i=0; i<item.length; i++) {
+        console.log(item[i]);
+
+        document.querySelector("#items").innerHTML += 
+        `<a href="./product.html?id=${item[i]._id}">
+            <article>
+                <img src=${item[i].imageUrl} alt=${item[i].altTxt}>
+                <h3 class="productName">${item[i].name}</h3>
+                <p class=${item[i].description}</p>
+            </article>
+        </a>`;
+    };
 };
-console.log(product[i]);
