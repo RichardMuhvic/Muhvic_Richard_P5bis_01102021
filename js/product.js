d18da802f6f3420fe1d495a94ee69b4456468bd5
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-12 11:25:29 +0100

    ajout de fetch dans productr.js

--- js/product.js
+++ js/product.js
@@ -3,3 +3,12 @@ let str = window.location.href;
 let url = new URL(str);
 let product = url.searchParams.get("id");
 console.log(product);
+
+fetch("http://localhost:3000/api/products/" + product)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    });
+
