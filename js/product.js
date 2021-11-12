aa5dde548583f1c1946160ca2a1409a383da7556
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-12 11:54:45 +0100

    création d'une fonction

--- js/product.js
+++ js/product.js
@@ -12,3 +12,28 @@ fetch("http://localhost:3000/api/products/" + product)
         console.log(data)
     });
 
+
+function name(params) {
+    //<div class="item__img">
+    //<img src="../images/logo.png" alt="Photographie d'un canapé"> -->
+    //</div>
+    let blockImage = document.getElementsByClassName('item_img');
+    blockImage.innerHTML = `<img src="${}" alt="${}">`
+
+    //<h1 id="title"><!-- Nom du produit --></h1>
+
+
+    //<span id="price"><!-- 42 --></span>
+
+
+    //<p id="description"><!-- Dis enim malesuada risus sapien gravida nulla nisl arcu. --></p>
+
+
+    //<select name="color-select" id="colors">
+        //<option value="">--SVP, choisissez une couleur --</option>
+            //<option value="vert">vert</option>
+        //<option value="blanc">blanc</option> -->
+    //</select>
+};
+
+
