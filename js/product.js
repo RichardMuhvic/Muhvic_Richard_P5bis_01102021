f28212018843403a34a8804977fecd0152d147e3
Richard Muhvic <richardmuhvic@gmail.com>
2021-11-12 15:48:24 +0100

    ajout de la boucle for pour le menu déroulant des couleurs

--- js/product.js
+++ js/product.js
@@ -1,4 +1,3 @@
-
 let str = window.location.href;
 let url = new URL(str);
 let product = url.searchParams.get("id");
@@ -19,30 +18,30 @@ function productSelected(items) {
     //récupération de l'image :
     let blockImage = document.querySelector('.item__img');
     blockImage.innerHTML = `<img src=${items.imageUrl} alt=${items.altTxt}>`;
-    console.log(blockImage);
 
     //récupération du titre :
     let blockTitle = document.getElementById('title');
     blockTitle.innerHTML = `<h1 id="title">${items.name}</h1>`;
-    console.log(blockTitle)
 
     //récupération du prix :
     let blockPrice = document.getElementById('price');
     blockPrice.innerHTML = `<span id="price">${items.price}</span>€</p>`;
-    console.log(blockPrice)
 
     //récupération de la description :
     //<p id="description"><!-- Dis enim malesuada risus sapien gravida nulla nisl arcu. --></p>
     let blockDescription = document.getElementById('description');
     blockDescription.innerHTML = `<p id="description">${items.description}</p>`;
-    console.log(blockDescription)
 
     //récupération des couleurs :
-    //<select name="color-select" id="colors">
-        //<option value="">--SVP, choisissez une couleur --</option>
-            //<option value="vert">vert</option>
-        //<option value="blanc">blanc</option> -->
-    //</select>
+    let blockColors = items.colors
+    for(let color of blockColors ) {
+        let optionColor = document.createElement("option");
+        optionColor.value = color;
+        optionColor.innerText = color;
+        document.getElementById("colors").appendChild(optionColor);
+    };
+    console.log(blockColors)
+    
 };
 
 
